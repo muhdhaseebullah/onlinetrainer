e13eb88378a027f9b1420454a82c9020a9d7a3c2
Muhammad Ismail <itsmuhammadismail@gmail.com>
2022-09-04 17:06:18 +0500

    ismail

---
+++ pages/workout_check.js
@@ -0,0 +1,124 @@
+import { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton, drawRect } from "../utils/utilities";
+
+let counter = 0;
+let stage = "down";
+
+function calculateAngle(a, b, c) {
+  let radians =
+    Math.atan2(c[1] - b[1], c[0] - b[0]) - Math.atan2(a[1] - b[1], a[0] - b[0]);
+  let angle = Math.abs((radians * 180.0) / Math.PI);
+
+  if (angle > 180.0) {
+    angle = 360 - angle;
+  }
+
+  return angle;
+}
+
+const WorkoutCheck = () => {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //  Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+    });
+    //
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      let shoulder = [pose[6].position.x, pose[6].position.y];
+      let elbow = [pose[8].position.x, pose[6].position.y];
+      let wrist = [pose[10].position.x, pose[10].position.y];
+
+      let angle = calculateAngle(shoulder, elbow, wrist);
+
+      if (angle > 160) {
+        stage = "down";
+      }
+      if (angle < 30 && stage === "down") {
+        stage = "up";
+        counter += 1;
+      }
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.6, ctx);
+    drawSkeleton(pose["keypoints"], 0.7, ctx);
+    drawRect(canvas, counter);
+  };
+
+  runPosenet();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+};
+
+export default WorkoutCheck;
